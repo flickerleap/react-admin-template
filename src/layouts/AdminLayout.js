9974169ed282c00767f5b53ebaf49dd943e68a23
Matthew Dalton <matt@flickerleap.com>
2018-03-23 15:09:55 +0200

    Updated Root and AdminLayout

--- src/layouts/AdminLayout.js
+++ src/layouts/AdminLayout.js
@@ -1,42 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, NavLink, Link } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import Header from "../components/layout/Header";
 import Nav from "../components/layout/Nav";
-import DashboardScreen from "../screens/DashboardScreen";
-import OutlineScreen from "../screens/Outline/OutlineScreen";
-import AddOutlineScreen from "../screens/Outline/AddOutlineScreen";
-import EditOutlineScreen from "../screens/Outline/EditOutlineScreen";
-import MeetingTypeScreen from "../screens/MeetingType/MeetingTypeScreen";
-import CampusScreen from "../screens/Campus/CampusScreen";
-import AddCampusScreen from "../screens/Campus/AddCampusScreen";
-import AddMeetingTypeScreen from "../screens/MeetingType/AddMeetingTypeScreen";
 
-export const AdminLayout = ({ 
-    routes = [], 
-    appConfig
-}) => (
+export const AdminLayout = ({routes = [], links = [], getComponent, appConfig}) => (
     <div>
         <Header {...appConfig}/>
         <div className='container-fluid'>
             <div className='row flex-xl-nowrap'>
                 <div className='col-lg-2 col-md-3 sidebar p-4 bg-light border-right'>
                     <div className='sidebar-sticky'>
-                        <Nav className='nav flex-column' links={
-                            routes.map((route) => {
-                                if(route.excludeFromNav === undefined || route.excludeFromNav !== true)
-                                {
-                                    return <NavLink className="nav-link" to={route.path}>{route.label}</NavLink>;
-                                }
-                            })
-                        } />
+                        <Nav className='nav flex-column' links={links}/>
                     </div>
                 </div>
                 <div className='col-lg-10 col-md-9 content p-4 bg-white'>
                     <Switch>
                         {
-                            routes.map((route, index) => (
-                                <Route key={index} path={route.path} component={route.component} exact={route.exact !== undefined ? route.exact : false} />
-                            ))
+                            routes.map((route, index) => {
+                                return (
+                                    <Route key={index} path={route.path} component={getComponent(route)}
+                                           exact={route.exact !== undefined ? route.exact : false}/>
+                                );
+                            })
                         }
                     </Switch>
                 </div>
@@ -44,3 +29,5 @@ export const AdminLayout = ({
         </div>
     </div>
 );
+
+export default AdminLayout;
