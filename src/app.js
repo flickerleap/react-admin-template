423c0d1a7ad595f167cd87c03f987053c82ffdd9
Matthew Dalton <matt@flickerleap.com>
2018-04-06 18:14:37 +0200

    Added style imports so that css/scss gets expoorted correctly Rebuilt css dist files

--- src/app.js
+++ src/app.js
@@ -51,3 +51,13 @@ export {defaultAppConfig} from './config/config';
 /** ROOT */
 export {Root} from './Root';
 
+// Styles
+// Import Flag Icons Set
+import 'flag-icon-css/css/flag-icon.min.css';
+// Import Font Awesome Icons Set
+import 'font-awesome/css/font-awesome.min.css';
+// Import Simple Line Icons Set
+import 'simple-line-icons/css/simple-line-icons.css';
+import '../scss/app.scss';
+import 'react-dates/initialize';
+
