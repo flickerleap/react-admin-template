e150e2711e114dce1054e678992f4ed6e28add06
Matthew Dalton <matt@flickerleap.com>
2018-05-09 20:58:49 +0200

    Made changes in Field file Updated build files

--- src/components/form/Field.js
+++ src/components/form/Field.js
@@ -7,12 +7,12 @@ export class Field extends React.Component {
         super(props);
 
         this.components = {
-            dropdown: DropDown,
-            date: Date,
-            textarea: TextArea,
-            time: Time,
-            datetime: DateTime,
-            select: Select,
+            dropdown: <DropDown {...props}/>,
+            date: <Date {...props}/>,
+            textarea: <TextArea {...props}/>,
+            time: <Time {...props}/>,
+            datetime: <DateTime {...props}/>,
+            select: <Select {...props}/>,
             custom: (props) => props.custom(props)
         };
     }
@@ -20,22 +20,22 @@ export class Field extends React.Component {
     getComponent = (type) => {
         let component = this.components[type];
         if (component === undefined) {
-            component = Input;
+            component = <Input  {...props}/>;
         }
 
         return component;
     };
 
-    getElement() {
+    /*getElement() {
         let props = this.props;
         if(props.ref !== undefined) {
             delete props.ref;
         }
         return React.createElement(this.getComponent(this.props.type), this.props);
-    }
+    }*/
 
     render() {
-        const element = this.getElement();
+        const element = this.getComponent(this.props.type);
 
         return (
             <div>
