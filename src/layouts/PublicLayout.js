ebd6f67a220fb47bf736a4d4db09a79d95d3ef27
Matthew Dalton <matt@flickerleap.com>
2018-07-31 11:01:28 +0200

    Updated PublicLayout component

--- src/layouts/PublicLayout.js
+++ src/layouts/PublicLayout.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
-export const PublicLayout = (props) => (
-    <div className="app flex-row align-items-center">
-        <div className="container">
-            <div className="justify-content-center row">
-                <div className="col col-md-8">
-                    <div className="card-group">
-                        <div className="p-4 card">
-                            <div className="card-body">
+export const PublicLayout = (routes = [], links = [], headerMenuItems = [], appConfig, getComponent, ...rest) => {
 
+    return (
+        <div className="app flex-row align-items-center">
+            <div className="container">
+                <div className="justify-content-center row">
+                    <div className="col col-md-8">
+                        <div className="card-group">
+                            <div className="p-4 card">
+                                <div className="card-body">
+
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
