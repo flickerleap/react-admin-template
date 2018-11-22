fae0a63eac454911717ba1a07aaf091f4b30b709
Matthew Dalton <matt@flickerleap.com>
2018-11-22 14:14:39 +0200

    Fixed bug in config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,17 @@
-import {PublicLayout} from "../layouts/PublicLayout";
-import {AdminLayout} from "../layouts/AdminLayout";
-import DefaultLogger from '../components/errors/DefaultLogger'
+import { PublicLayout } from '../layouts/PublicLayout'
+import { AdminLayout } from '../layouts/AdminLayout'
+import { DefaultLogger } from '../components/loggers/loggers'
 
 export const defaultAppConfig = {
-    title: 'Admin',
-    layouts: {
-        public: PublicLayout,
-        admin: AdminLayout
-    },
-    copyright: {
-        url: 'https://www.flickerleap.com',
-        name: 'Flicker Leap',
-        company: 'Flicker Leap'
-    },
-    logger: new DefaultLogger()
-};
+  title: 'Admin',
+  layouts: {
+    public: PublicLayout,
+    admin: AdminLayout
+  },
+  copyright: {
+    url: 'https://www.flickerleap.com',
+    name: 'Flicker Leap',
+    company: 'Flicker Leap'
+  },
+  logger: new DefaultLogger()
+}
