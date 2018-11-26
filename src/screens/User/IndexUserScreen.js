54a809f2c40dd674543d7051c53a2f1608a692cb
Matthew Dalton <matt@flickerleap.com>
2018-11-26 11:51:54 +0200

    Updated file formatting Added functionality to DataTable and IndexScreen to only show the FilterBar if the `showFilters` prop is set to true Updated IndexUserScreen to test the new show/hide filtering fucntionality

--- src/screens/User/IndexUserScreen.js
+++ src/screens/User/IndexUserScreen.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {userModel as model} from "../../models/models";
-import {IndexScreen} from "../../components/screens/IndexScreen";
-import {fetchExamples, removeExample} from "../../store/actions/actions";
-import {userItems} from "../../models/user";
+import React from 'react'
+import { connect } from 'react-redux'
+import { userModel as model } from '../../models/models'
+import { IndexScreen } from '../../components/screens/IndexScreen'
+import { fetchExamples, removeExample } from '../../store/actions/actions'
+import { userItems } from '../../models/user'
 
 const mapStateToProps = (state) => {
-    return {
-        items: userItems,
-        loading: false,
-        pagination: {
-            total: 1,
-            perPage: 10,
-            current: 1,
-            link: ''
-        },
-    };
-};
+  return {
+    items: userItems,
+    loading: false,
+    pagination: {
+      total: 1,
+      perPage: 10,
+      current: 1,
+      link: ''
+    },
+  }
+}
 
 const mapDispatchToProps = (dispatch) => ({
-    fetch: (params) => dispatch(fetchExamples(params)),
-    remove: (id) => dispatch(removeExample(id))
-});
+  fetch: (params) => dispatch(fetchExamples(params)),
+  remove: (id) => dispatch(removeExample(id))
+})
 
 const initialProps = {
-    title: model.plural,
-    fields: model.getDisplayFields()
-};
+  title: model.plural,
+  fields: model.getDisplayFields(),
+  showFilters: true
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)((props) => {
-    const actions = model.getActions(props);
-    const finalProps = {...props, ...initialProps, actions};
-    return <IndexScreen {...finalProps} />;
-});
+  const actions = model.getActions(props)
+  const finalProps = {...props, ...initialProps, actions}
+  return <IndexScreen {...finalProps} />
+})
